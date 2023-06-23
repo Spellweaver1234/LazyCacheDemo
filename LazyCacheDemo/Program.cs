e7ec59ffa23ac359326c8dc3284588c582de4e9e
Arseniy (windows) <Arsenchozzz@mail.ru>
2023-06-23 17:33:16 +0300

    ref

--- LazyCacheDemo/Program.cs
+++ LazyCacheDemo/Program.cs
@@ -6,7 +6,7 @@ ProductHandler productHandler = new();
 productHandler.InitReset();
 
 // get data from cache
-Product product1 = productHandler.GetProduct("product-06");
+Product product1 = productHandler.GetProduct("product-05");
 Product product2 = productHandler.GetProduct("product-01");
 
 Console.WriteLine("Product Name : " + product1.Name + "    Product Price : " + product1.Price);
